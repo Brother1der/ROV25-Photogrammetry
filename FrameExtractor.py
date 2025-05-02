a865d3ef8ce3f40e9cd0df7158bf8631b9887ddb
Graham <grahamsw10@gmail.com>
2025-05-02 08:49:31 -0500

    Update FrameExtractor.py

--- FrameExtractor.py
+++ FrameExtractor.py
@@ -1,13 +1,29 @@
 from moviepy.editor import VideoFileClip
 import os
-import subprocess #possibly starts a vbs file
+import subprocess # possibly starts a vbs file
 import keyboard
+import pyautogui
+import cv2
+import numpy as n2
+import time
 
-videoFile = r'replace_with_dir'
-imageDirectory = r'replace_with_dir'
+resolution = (1920, 1080)
+codec = cv2.VideoWriter_fourcc(*mp4v)
+filename = "Recording.mp4" # names file
+fps = 30.0 # fps of file
+out = cv2.VideoWriter(filename, codec, fps, resolution)
+
+def video_capture_loop(seconds):
+    start_time = time.time()
+    while (time.time() - start_time) < seconds:
+           img = pyautogui.screenshot()
+    frame = np.array(img) # Screenshot to numpy array
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Convert from BGR to RGB
+    out.write(frame) # Writes to output file
 
 def start_hotkey(): # Creates the hotkey's function
     print("Hotkey pressed")
+    video_capture_loop(30) # Calls video capture loops function
     extract_frames(video, imageDirectory) # Calls extraction function
 
 def extract_frames(video, imageDirectory):
@@ -24,6 +40,9 @@ def extract_frames(video, imageDirectory):
             imagepath = os.path.join(imageDirectory, frame_filename)
             video.save_frame(imagepath, t)
             print(f"Saved frame at {t} seconds: {imagepath}")
+    # Run VBS file
+    subprocess.run(["cd", "C:\Windows\Scripts"], shell=True)
+    subprocess.run(["cscript scriptname.vbs"], shell=True)
 
 # Create the VideoFileClip object
 video = VideoFileClip(videoFile)
@@ -31,12 +50,7 @@ video = VideoFileClip(videoFile)
 # Creates the hotkey
 keyboard.add_hotkey('ctrl+shift+a', start_hotkey)
 
-# Run VBS file
-subprocess.run(["cd", "C:\Windows\Scripts"], shell=True)
-subprocess.run(["cscript scriptname.vbs"], shell=True)
-
 keyboard.wait('esc')  # Wait for the 'esc' key to be pressed to exit the program
 
 # Close the video clip to release resources
 video.close()
-print("test")
