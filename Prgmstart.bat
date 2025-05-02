5aef66cb6122925a7d071da1d0d811ca0836d266
Graham <grahamsw10@gmail.com>
2025-05-01 19:40:09 -0500

    Commented for future children.

--- Prgmstart.bat
+++ Prgmstart.bat
@@ -1,1 +1,2 @@
-"C:\Program Files\PTGui\ptgui.exe" -stitchnogui "C:\Path\To\Projectfile.pts"
+"C:\Program Files\PTGui\ptgui.exe" -stitchnogui "C:\Path\To\Projectfile.pts" 
+:: Above line starts PTGUI with the given directory to stitch with
