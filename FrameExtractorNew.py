5428c9f79633777b8d7a6bca3c692704a7edf9b9
Graham <grahamsw10@gmail.com>
2025-05-22 15:04:22 -0500

    Update FrameExtractorNew.py

---
+++ FrameExtractorNew.py
@@ -0,0 +1,18 @@
+import os
+import subprocess
+import time
+import keyboard
+import pyautogui
+
+fps = 6
+seconds = 5
+output_dir = r"C:\Example\Screenshots"
+ss_num = 1
+
+def get_screenshots(fps, sec, output_dir, ss_num):
+    start_time = time.time()
+    while (time.time() - start_time) < seconds:
+        if time.time() % (1/fps(ss_num)) == 0:
+            screenshot = pyautogui.screenshot()
+            screenshot.save(os.path.join(output_dir, f"screenshot_{int(time.time())}.png"))
+            ss_num += 1
